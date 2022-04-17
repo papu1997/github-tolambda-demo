adcd04f36a2da7cf5b9bc0e9111a3a82aec7d716
papu1997 <papu.srikanta1997@gmail.com>
2022-04-17 20:15:08 +0530

    commit2

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "my-first-ecr-repo" # Naming my repository
+}
